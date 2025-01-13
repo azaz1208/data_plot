04468be47e5b2342e0d35adc27c3607a78d236d7
azaz1208 <angela87120837@gmail.com>
2025-01-14 00:08:19 +0800

    20250114 00:08

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, jsonify, session
 from flask_session import Session
+import plotly.graph_objects as go
 import pandas as pd
 
 app = Flask(__name__)
@@ -18,7 +19,7 @@ def data():
         f = request.files["user_file"]
         rt_content["Your file is!"] = f.filename
         df = pd.read_excel(f)
-        print(df)
+        # print(df)
         print(list(df.columns))
         session['data'] = df.to_dict()
         return render_template("data2.html", col=list(df.columns))
@@ -29,8 +30,27 @@ def printSTH():
     return df_dict
 
 @app.route('/plot', methods=['POST'])
+def user_plot():
+    x = request.form["X"]  # x 是Student ID
+    y = request.form["Y"]  # y 是English
+    df_data = pd.DataFrame.from_dict(session['data'])
+    fig = go.Figure()
+    fig.add_trace(
+        go.Scatter(
+            x = list(df_data[x]),
+            y = list(df_data[y]),
+            mode = "lines+markers"
+        )
+    )
+    fig.update_layout(
+        title = f"{x} vs {y} "
+    )
+    fig.write_html("templates/test_plot.html") 
+    return render_template("test_plot.html")
+    # return jsonify({x:list(df_data[x]),y:list(df_data[y])})
+    
 
 
 
 if __name__ == '__main__':
-    app.run(debug= True)
+    app.run(debug = True)
